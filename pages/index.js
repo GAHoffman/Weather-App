feefebc56141c5e421107fde3f6f498b6b633e4f
Garrett Hoffman <son0hoff@gmail.com>
2023-05-28 22:11:43 -0700

    Updated for Weather component display box for current weather in city and icons from open weather map

--- pages/index.js
+++ pages/index.js
@@ -42,7 +42,7 @@ export default function Home() {
         {/* background image */}
         <Image
           src="https://images.unsplash.com/photo-1601134467661-3d775b999c8b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1075&q=80"
-          layout="fill"
+          fill
           className="object-cover"
           alt=""
         />
